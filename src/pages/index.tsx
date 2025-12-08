471abf95c1c3280c85b1cdd6d75644fdc5a0e0f2
zoomdong <zoomdong@U-0K627Y2C-2335.local>
2025-12-08 14:16:18 +0800

    chore: add hmr minimal demo

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import yayJpg from '../assets/yay.jpg';
+import "antd/lib/config-provider/style/index.js";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>Yay! Welcome to umi!</h2>
+      <p>
+        <img src={yayJpg} width="388" />
+      </p>
+      <p>
+        To get started, edit <code>pages/index.tsx</code> and save to reload.
+      </p>
+    </div>
+  );
+}
