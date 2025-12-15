ca7e082f01e27beb8feab506efaf2018338a5d4d
fireairforce <1344492820@qq.com>
2025-12-15 10:33:18 +0800

    chore: allow dynamic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import yayJpg from '../assets/yay.jpg';
 // import "antd/lib/config-provider/style/index.js";
 require('./a.css');
+// import * as onig from 'vscode-oniguruma/release/onig.wasm';
+
+// onig;
+
+// const onig2 = new URL("https://unpkg.com/vscode-oniguruma@2.0.1/release/onig.wasm");
+
+// onig2;
 
 export function requireStat(name: string) {
-  return require(require.resolve(`${name}/package.json'`))
+  return require(`${name}`);
 }
 
 export async function importUrl(url: string) {
